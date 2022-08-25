867c28557d831a928bad49aa70ee1e9d9f07d092
TrasherDK <trasherdk@fumlersoft.dk>
2022-08-25 06:57:19 +0200

    deps: add `cors`
    deps: bump `ejs@3.1.8`
    
    Minor tweaks on `server.js`
    
    format: standard.js

--- public/client.js
+++ public/client.js
@@ -1,18 +1,17 @@
 // client-side js
 
 document.addEventListener('DOMContentLoaded', () => {
-  var input = document.querySelector('#input')
-  var submit = document.querySelector('#submit')
-  var hostname = window.location.hostname
-  var protocol = window.location.protocol
-  var port = window.location.port ? `:${window.location.port}`: ''
-  
+  const input = document.querySelector('#input')
+  const submit = document.querySelector('#submit')
+  const hostname = window.location.hostname
+  const protocol = window.location.protocol
+  const port = window.location.port ? `:${window.location.port}` : ''
+
   submit.addEventListener('click', event => {
     event.preventDefault()
-    var value = String(input.value).replace('/', '-')
+    const value = String(input.value).replace('/', '-')
     // Set the browser location, using the encoded input value as the
     // first URL parameter.
-    window.location.href = encodeURI(`${protocol}//${hostname}${port}/${value}`);
+    window.location.href = encodeURI(`${protocol}//${hostname}${port}/${value}`)
   })
 })
-
--- server.js
+++ server.js
@@ -1,50 +1,59 @@
 // server.js
 
-var express = require('express');
-var app = express();
+const express = require('express')
+const app = express()
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
-var cors = require('cors');
-app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
+const cors = require('cors')
+app.use(cors({ optionSuccessStatus: 200 })) // some legacy browsers choke on 204
 
 // Use ejs for html templates
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
 // static files are served from public
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 // The root url servers the demo homepage
-app.get('/', function(request, response) {
-  response.render('index', { date: null, url: 'home' });
-});
+app.get('/', function (request, response) {
+  response.render('index', { date: null, url: 'home' })
+})
 
 // Handles get requests that include at least one param
-app.get('API/timestamp/:date', function(request, response) {
-  const { params, query } = request;
+app.get('/:date', function (request, response) {
+  const { params } = request
   const options = {
     weekday: 'long',
     year: 'numeric',
     month: 'long',
     day: 'numeric'
-  };
-  let date = new Date(params.date);
+  }
+  let date = params.date === 'now' ? new Date().getTime() : new Date(params.date)
 
   // Handle unix timestamp,
   if (!/[^\d]/.test(params.date)) {
-    date = new Date(parseInt(params.date) * 1000);
+    date = new Date(parseInt(params.date) * 1000)
   }
 
-  let natural = date.toLocaleDateString('en-us', options);
-  let unix = date.getTime() / 1000;
+  date = (date instanceof Date) ? date : new Date(parseInt(date))
+
+  let natural
+  try {
+    natural = date.toLocaleDateString('en-us', options)
+  } catch (error) {
+    console.log(error)
+  }
+  const unix = date.getTime() / 1000
   // Sends the JSON response
   response.json({
+    iso: date.toISOString(),
     unix: unix || null,
     natural: natural === 'Invalid Date' ? null : natural
-  });
-});
+  })
+})
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+const listener = app.listen(process.env.PORT || 8080, 'localhost', function () {
+  const { address, port } = listener.address()
+  console.log(`Your app is listening on http://${address}:${port}`)
+})
